a56b663f46da7eab573e3b09a96e736b0f447a98
Oliwans <wpking2008@gmail.com>
2022-05-18 15:50:24 +0800

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,46 @@
 <!--
  * @Date: 2022-05-18 14:09:46
  * @LastEditors: wangpeng
- * @LastEditTime: 2022-05-18 14:14:02
+ * @LastEditTime: 2022-05-18 15:50:10
  * @FilePath: /arithmetic/readme.md
 -->
-### 前端面试及算法
+### 前端面试及算法 JavaScript版
 #### 前端手写代码题
-[普通链接](./src/font-end-write/index.js)
+[手写代码](./src/font-end-write/index.js)
+
+#### 算法 easy
+[二分查找](./src/binary-search/index.js)
+[四种排序](./src/sort-collection/index.js)
+
+[反转二叉树](./src/invert-tree/index.js)
+[二叉树前、中、后序遍历](./src/binary-search-tree/index.js)
+
+[括号匹配](./src/brackets-match/index.js)
+[输出连续字符及次数](./src/continue-number/index.js)
+[数字千分位化](./src/count-thousands/index.js)
+[斐波那契数列](./src/fibonacci/index.js)
+[查找n之前的对称数(回文)](./src/find-symmetric-num/index.js)
+[字母大小写转化](./src/letter-upper-lower/index.js)
+[最富有资产总数](./src/maximum-wealth/index.js)
+[合并数组并排序](./src/merge-array/index.js)
+[数组中的0移动至末尾](./src/move-zero/index.js)
+[链表实现队列](./src/queue-with-link/index.js)
+[翻转单向链表](./src/reverse-link-list/index.js)
+[旋转数组k步](./src/rotate-k/index.js)
+[买卖股票最佳时机](./src/sales-stocks/index.js)
+[数组中和为n的两个数](./src/two-number-sum-index/index.js)
+[数组中和为n的两个数索引](./src/rotate-k/index.js)
+[两个栈实现一个队列](./src/two-stack-one-queue/index.js)
+
+#### 算法 middle
+[括号生成](./src/add-brackets/index.js)
+[删除链表第n个节点](./src/delete-link-n/index.js)
+[数组中第K大的数](./src/find-kth-largest/index.js)
+[搜索二维矩阵](./src/find-number-in-2d/index.js)
+[电话号码的字母组合](./src/find-phone-number/index.js)
+[出现频率最高的字符](./src/highest-frequency-char/index.js)
+[最长回文子串](./src/longest-palindrome/index.js)
+[不重复最长子字符串长度](./src/longest-substring/index.js)
+[盛水最多的容器](./src/max-water/index.js)
+[三数之和](./src/three-number-sum/index.js)
+[两数相加](./src/two-number-sum-link/index.js)
--- src/sort-collection/index.js
+++ src/sort-collection/index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-04-11 18:37:19
  * @LastEditors: wangpeng
- * @LastEditTime: 2022-04-11 20:53:25
+ * @LastEditTime: 2022-05-18 15:35:46
  * @FilePath: /arithmetic/src/sort-collection/index.js
  */
 /**
@@ -20,6 +20,73 @@ function quickSort(arr) {
         e >= flag ? right.push(e) : left.push(e)
     });
     return quickSort(left).concat([flag],quickSort(right))
+}
+/**
+ * @description: 冒泡排序
+ * @param {*} arr
+ * @return {*}
+ */
+function bubbleSort(arr) {
+    //console.time('BubbleSort');
+    // 获取数组长度，以确定循环次数。
+    let len = arr.length;
+    // 遍历数组len次，以确保数组被完全排序。
+    for(let i=0; i<len; i++) {
+        // 遍历数组的前len-i项，忽略后面的i项（已排序部分）。
+        for(let j=0; j<len - 1 - i; j++) {
+            // 将每一项与后一项进行对比，不符合要求的就换位。
+            if(arr[j] > arr[j+1]) {
+                [arr[j+1], arr[j]] = [arr[j], arr[j+1]];
+            }
+        }
+    }
+    //console.timeEnd('BubbleSort');
+    return arr;
+}
+/**
+ * @description: 选择排序
+ * @param {*} arr
+ * @return {*}
+ */
+function selectionSort(arr) {
+    //console.time('SelectionSort');
+    // 获取数组长度，确保每一项都被排序。
+    let len = arr.length;
+    // 遍历数组的每一项。
+    for(let i=0; i<len; i++) {
+        // 从数组的当前项开始，因为左边部分的数组项已经被排序。
+        let min = i;
+        for(let j=i; j<len; j++) {
+            if(arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if(min !== i) {
+            [arr[min], arr[i]] = [arr[i], arr[min]];
+        }
+    }
+    //console.timeEnd('SelectionSort');
+    return arr;
+}
+/**
+ * @description: 插入排序
+ * @param {*} arr
+ * @return {*}
+ */
+function insertionSort(arr) {
+    //console.time('InsertionSort');
+    let len = arr.length;
+    for(let i=1; i<len; i++) {
+        let j = i;
+        let tmp = arr[i];
+        while(j > 0 && arr[j-1] > tmp) {
+            arr[j] = arr[j-1];
+            j--;
+        }
+        arr[j] = tmp;
+    }
+    //console.timeEnd('InsertionSort');
+    return arr;
 }
 
 module.exports = quickSort
